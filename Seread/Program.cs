44e5a18d0ea906180667715265615c418958c7d0
Daniel Shroff <danielshroff@hotmail.com>
2018-03-24 22:13:02 +0530

    Initial commit

---
+++ Seread/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Seread
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
